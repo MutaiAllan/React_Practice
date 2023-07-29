8fce6994cc609aca497129235ab45a6985889cf3
MutaiAllan <MutaiAllan>
2023-07-29 19:15:03 +0300

    Added Search box

--- robot/src/App.js
+++ robot/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import CardList from './CardList';
 import { robots } from './robots';
+import SearchBox from "./SearchBox";
 
 function App () {
     return (
         <>
         <h1>RoboFriends</h1>
+        <SearchBox />
         <CardList robots={robots}/>
         </>
     )
